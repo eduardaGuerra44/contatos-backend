7d0242707cdef7acce65d201aa153ba84784d8d9
Eduarda Guerra <eduardaguerra44@gmail.com>
2022-09-21 19:11:08 -0300

    aprendendo eventos e manipulação de DOM

--- public/js/home.js
+++ public/js/home.js
@@ -15,10 +15,33 @@ const inputSenha = document.querySelector("#form input[type=password]")
 const buttonEntrar = document.querySelector("#form button[type=submit]")
 
 
-//Associar o evento "perder o foco" com uma função que verifica se o campo foi preenchido"
-inputEmail.addEventListener(
-   'blur', 
-   ()=>{
 
+// informações sobre o evento 
+const imprimeEvento = (evento) => {
+   console.log(evento.target)
+   console.log(evento.type)
+}
+
+
+//Associar o evento "perder o foco" com uma função que verifica se o campo foi preenchido"
+inputEmail.addEventListener('blur', imprimeEvento);
+inputSenha.addEventListener('blur', imprimeEvento);
+
+form.addEventListener(
+   'submit', 
+   (evento) => {
+      // inibir o envio do formulário
+      evento.preventDefault();
+
+      //verificar se o email digitado é igual ao emailEsperado
+      if(inputEmail.value != emailEsperado){
+         alert("Falha no login");
+         return;
+      }
+
+      //verificar se a senha digitada é igual ao senhaEsperada
+
+      //se estiver tudo ok, emitir mensagem de sucesso
+      alert("login deu certo"); 
    }
-);
+)
